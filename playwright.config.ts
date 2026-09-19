c40d9cf3ba1f40a2cb34987f1a8330acf9555971
mbayangxo <goldendaffodilxo@gmail.com>
2026-09-19 18:52:20 -0400

    ci: use installed playwright test runner

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from "@playwright/test";
+import { defineConfig, devices } from "playwright/test";
 
 const baseURL = process.env.KEBU_E2E_BASE_URL ?? "http://127.0.0.1:3000";
 
